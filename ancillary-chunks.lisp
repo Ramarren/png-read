fbee2f18dffbc1d252f6b3a51a4200ed406556e1
Ramarren <ramarren@gmail.com>
2008-04-08 15:41:46 +0200

    Add fallback methods.

--- ancillary-chunks.lisp
+++ ancillary-chunks.lisp
@@ -1,1 +1,5 @@
 (in-package :png-read)
+
+(defgeneric parse-ancillary-chunk (chunk-type chunk-data)
+  (:method (chunk-type chunk-data)
+    (warn "Unknown ancillary chunk.")))
--- critical-chunks.lisp
+++ critical-chunks.lisp
@@ -1,1 +1,5 @@
 (in-package :png-read)
+
+(defgeneric parse-critical-chunk (chunk-type chunk-data)
+  (:method (chunk-type chunk-data)
+    (error "Unknown critical chunk.")))
