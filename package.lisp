506e68eb4e796c3b240efa938561a77df07a29a1
Ramarren <ramarren@gmail.com>
2008-04-08 19:25:17 +0200

    Export some symbols.

--- package.lisp
+++ package.lisp
@@ -1,2 +1,6 @@
 (defpackage #:png-read
-  (:use #:cl #:iterate #:chipz))
+  (:use #:cl #:iterate #:chipz)
+  (:export #:read-png-file #:read-png-datastream
+	   #:png-state
+	   #:image-data #:width #:height #:bit-depth
+	   #:colour-type #:compression #:filter-method #:interlace-method #:pallete))
