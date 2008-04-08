8fddc2b73bd01d2bb48ec30df61a35c3edcfdcc8
Ramarren <ramarren@gmail.com>
2008-04-08 19:34:33 +0200

    Cleanup.

--- package.lisp
+++ package.lisp
@@ -3,4 +3,4 @@
   (:export #:read-png-file #:read-png-datastream
 	   #:png-state
 	   #:image-data #:width #:height #:bit-depth
-	   #:colour-type #:compression #:filter-method #:interlace-method #:pallete))
+	   #:colour-type))
