dcb3cf25fb97eb3ea6555b4bc4389cb7d235af54
Ramarren <ramarren@gmail.com>
2008-04-08 14:59:23 +0200

    Identify header.

---
+++ basic-chunks.lisp
@@ -0,0 +1,15 @@
+(in-package :png-read)
+
+(defun read-png-file (file)
+  (with-open-file (png-stream file :direction :input :element-type '(unsigned-byte 8))
+    (read-png-datastream png-stream)))
+
+(defvar *png-header* #(137 80 78 71 13 10 26 10))
+
+(defun read-png-datastream (png-stream)
+  (let ((header (make-array (length *png-header*) :element-type '(unsigned-byte 8))))
+    (read-sequence header png-stream)
+    (cond
+      ((every #'eql *png-header* header)
+       (read-png-chunks png-stream))
+      (error "Not PNG file."))))
