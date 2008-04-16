f9173046bafa303b12f8178534a5d53373800165
Ramarren <ramarren@gmail.com>
2008-04-16 16:22:51 +0200

    Add proper tRNS handling for indexed images.

--- ancillary-chunks.lisp
+++ ancillary-chunks.lisp
@@ -12,37 +12,44 @@
 	(h (height png-state))
 	(ct (colour-type png-state))
 	(imd (image-data png-state))
+	(idx (index-data png-state))
 	(transp (transparency png-state)))
    (let ((t-map (make-array (list w h))))
      (iter (for i from 0 below w)
 	   (iter (for j from 0 below h)
 		 (setf (aref t-map i j)
 		       (ecase ct
-			 (:greyscale (if (eql (aref imd i j) transp)
-				0
-				255))
-			 (:truecolor (if (every #'identity
-				       ;strange... SBCL hangs during compilation when
-				       ;           always iterate keyword is used
-				       (iter (for k from 0 to 2)
-					     (collect (eql (aref imd i j k)
-							   (aref transp k)))))
-				0
-				255))))))
+			 (:greyscale
+			  (if (eql (aref imd i j) transp)
+			      0
+			      255))
+			 (:truecolor
+			  (if (every #'identity
+				     ;;strange... SBCL hangs during compilation when
+				     ;;           always iterate keyword is used
+				     (iter (for k from 0 to 2)
+					   (collect (eql (aref imd i j k)
+							 (aref transp k)))))
+			      0
+			      255))
+			 (:indexed-colour
+			  (if (array-in-bounds-p transp (aref idx i j))
+			   (setf (aref t-map i j)
+				 (aref idx i j))
+			   (setf (aref t-map i j)
+				 255)))))))
      (setf (transparency png-state) t-map))))
 
 (defmethod parse-ancillary-chunk ((chunk-type (eql '|tRNS|)) chunk-data)
-  (when (or (eql (interlace-method *png-state*) :no-interlace)
-	    (not (eql (colour-type *png-state*) :indexed-colour)))
-   (ecase (colour-type *png-state*)
-     (:greyscale (setf (transparency *png-state*)
-		       (big-endian-vector-to-integer chunk-data)))
-     (:truecolor (setf (transparency *png-state*)
-		       (vector (big-endian-vector-to-integer (subseq chunk-data 0 2))
-			       (big-endian-vector-to-integer (subseq chunk-data 2 4))
-			       (big-endian-vector-to-integer (subseq chunk-data 4 6)))))
-     (:indexed-colour (setf (transparency *png-state*)
-			    chunk-data))))
+  (ecase (colour-type *png-state*)
+    (:greyscale (setf (transparency *png-state*)
+		      (big-endian-vector-to-integer chunk-data)))
+    (:truecolor (setf (transparency *png-state*)
+		      (vector (big-endian-vector-to-integer (subseq chunk-data 0 2))
+			      (big-endian-vector-to-integer (subseq chunk-data 2 4))
+			      (big-endian-vector-to-integer (subseq chunk-data 4 6)))))
+    (:indexed-colour (setf (transparency *png-state*)
+			   chunk-data)))
   (when (or (eql (colour-type *png-state*) 0)
 	    (eql (colour-type *png-state*) 2))
     (push #'build-transparency-map (postprocess-ancillaries *png-state*))))
