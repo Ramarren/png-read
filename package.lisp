385e18344a8369e80d59eb043c8c2b0c6fb7b40a
Ramarren <ramarren@gmail.com>
2008-06-18 13:14:16 +0200

    Export restart name.

--- package.lisp
+++ package.lisp
@@ -1,7 +1,7 @@
 (defpackage #:png-read
   (:use #:cl #:iterate #:chipz #:babel)
   (:export #:read-png-file #:read-png-datastream
-	   #:png-state #:crc-failure #:*crc-fail-behaviour*
+	   #:png-state #:crc-failure #:*crc-fail-behaviour* #:ignore-crc-failure
 	   #:image-data #:width #:height #:bit-depth
 	   #:colour-type #:palette
 	   ;ancillaries
